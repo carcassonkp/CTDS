d7da962b0bd7630bbfd5461f8a5bd36c436efd12
carcassonkp <carcassonkp@hotmail.com>
2021-12-23 16:12:43 +0000

    finished script

--- TPC1_16998.py
+++ TPC1_16998.py
@@ -11,15 +11,8 @@ headers = {'Authorization': 'Basic %s' % userAndPass}
 
 # request to the API
 
-
-counties_Braga = ["AMARES", "BARCELOS", "BRAGA", "CABECEIRAS DE BASTO", "CELORICO DE BASTO",
-                  "ESPOSENDE", "FAFE",
-                  "GUIMARÃES", "PÓVOA DE LANHOSO", "TERRAS DE BOURO", "VIEIRA DO MINHO", "VILA NOVA DE FAMALICÃO",
-                  "VILA VERDE",
-                  "VIZELA"]
-
 folder = "Covid2021"
-file_name = "Covid_Braga_test.json"
+file_name = "Covid_Braga.json"
 
 if not os.path.exists(folder):  # criar pasta caso não exista
     os.mkdir(folder)
@@ -35,23 +28,21 @@ end_date = "21-12-2021"
 start_date_obj = datetime.strptime(start_date, date_format)
 end_date_obj = datetime.strptime(end_date, date_format)
 
-# r = requests.get(f'https://covid19-api.vost.pt/Requests/get_county_list/', headers=headers)
-r_all_counties_by_date = requests.get("https://covid19-api.vost.pt/Requests/get_entry_counties/{}_until_{}".format(start_date, end_date), headers=headers)
-# api_url =
-# r_all_counties_by_date = requests.get(api_url),headers=headers
-# print(r_all_counties_by_date.json())
 
-cnt = 0
+r_all_counties_by_date = requests.get("https://covid19-api.vost.pt/Requests/get_entry_counties/{}_until_{}".format(start_date, end_date), headers=headers)
+
+my_keys = ['data', 'concelho', 'confirmados_1']
+Data_to_store=[]
+
 for entry in r_all_counties_by_date.json():
     compare_date_obj = datetime.strptime(entry['data'], date_format)
-    # print(entry['data'])
-    # print(compare_date_obj)
-    # if entry['concelho'] in counties_Braga and entry['data'].endswith("2021"):
-    if entry['concelho'] in counties_Braga and start_date_obj <= compare_date_obj <= end_date_obj:
-        cnt += 1
-        # print(entry['data'])
-        # print(entry['concelho'])
-        with open(file_path, "a") as fp:
-            json.dump(entry, fp)
+    if entry['distrito']=='BRAGA' and start_date_obj <= compare_date_obj <= end_date_obj:
+        entry_1 = {k: v for (k, v) in entry.items() if k in my_keys}
+        Data_to_store.append(entry_1)
 
-print(cnt)
+with open(os.path.join(folder,file_name), "a") as fp:
+    json.dump(Data_to_store, fp)
+
+
+
+
